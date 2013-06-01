f7dbff1d334a8c0e0a8f002587a71ee720f14995
C?dric Krier <ced@b2ck.com>
2013-06-01 19:59:32 +0200

    Increase version number

--- vatnumber/__init__.py
+++ vatnumber/__init__.py
@@ -6,7 +6,7 @@ http://sima-pc.com/nif.php
 http://en.wikipedia.org/wiki/Vat_number
 '''
 
-__version__ = '1.1'
+__version__ = '1.2'
 VIES_URL = 'http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl'
 
 
